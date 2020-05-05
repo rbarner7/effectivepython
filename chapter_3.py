b54279b4fd640a62d9c3de1f37b0071bb8b9707a
Ryan Barner <rbarner@lyft.com>
2020-05-05 01:06:38 -0700

    Wrapping up Item 21
    Signed-off-by: Ryan Barner <rbarner@lyft.com>

--- chapter_3.py
+++ chapter_3.py
@@ -47,6 +47,74 @@ def main():
 
     # >>> Result is 2.5
 
+    # Item 21: Know How Closures Interact with Variable Scope
+    def sort_priority(values, group):
+        def helper(x):
+            if x in group: # closure function
+                return (0, x)
+            return (1, x)
+
+        values.sort(key=helper) # sort method accepts helper (a closure function) as the key argument
+
+    numbers = [8, 3, 1, 2, 5, 4, 7, 6]
+    group = {2, 3, 5, 7}
+    sort_priority(numbers, group)
+    print(numbers)
+
+    # >>> [2, 3, 5, 7, 1, 4, 6, 8]
+
+    # Scope demonstration
+    def sort_priority2(numbers, group):
+        found = False # Scope: 'sort_priority2'
+
+        def helper(x):
+            if x in group:
+                found = True  # new variable, not a reassignment as expected; scope: 'helper'
+                return (0, x)
+            return (1, x)
+
+        numbers.sort(key=helper)
+        return found
+
+    found = sort_priority2(numbers, group)
+    print('Found:', found)
+    print(numbers)
+
+    # >>> Found: False
+    # [2, 3, 5, 7, 1, 4, 6, 8]  # hoped for True, but did not get it due to scope
+    # found trapped in helper function in which it was defined (scope: helper)
+
+    # using nonlocal statement
+    def sort_priority3(numbers, group):
+        found = False
+
+        def helper(x):
+            nonlocal found  # Added nonlocal statement
+            if x in group:
+                found = True
+                return (0, x)
+            return (1, x)
+
+        numbers.sort(key=helper)
+        return found
+
+    #Better: wrap state in helper class when nonlocal gets complicated
+    class Sorter:
+        def __init__(self, group):
+            self.group = group
+            self.found = False
+
+        def __call__(self, x):
+            if x in self.group:
+                self.found = True
+                return (0, x)
+
+        return (1, x) # parantheses are redundant, can be removed
+
+    sorter = Sorter(group)
+    numbers.sort(key=sorter)
+    assert sorter.found is True
+
 
 ##############################################################
 if __name__ == "__main__":
