995712dd55e6b339cc25a3eecec8c1d85f894d20
Ryan Barner <rbarner@lyft.com>
2020-05-05 14:29:12 -0700

    Adding item 23 intro
    Signed-off-by: Ryan Barner <rbarner@lyft.com>

--- chapter_3.py
+++ chapter_3.py
@@ -143,7 +143,7 @@ def main():
     # >>> (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
 
     # Item 23: Provide Optional Behavior with Keyword Arguments
-    
+
 
 ##############################################################
 if __name__ == "__main__":
